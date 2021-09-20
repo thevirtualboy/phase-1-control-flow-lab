a4d3aac3d7a9b8d35d0da0d8214822863d9baf27
Peter Fontana <dude0508@gmail.com>
2021-09-20 12:44:59 -0400

    commit tree 124e138c4ad165bc856efc80bdd081a6794cd10d

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400) {
+    return ('This one is on me!');
+  } else if (someNumber >= 2001 && someNumber < 2501) {
+    return ('I will gladly take your thirty bucks.');
+  } else if (someNumber >= 2501) {
+    return ('No can do.');
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let cityResponse = city === 'NYC' ? (cityResponse = 'Ok, sounds good.') : (cityResponse = 'No go.');
+  return cityResponse;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generosity){
+  switch (generosity) {
+    case generosity = 'generous':
+      return ('Thank you so much.');
+      break;
+    case generosity = 'not as generous':
+      return ('Thank you.');
+      break;
+    default:
+      return ('Bye.');
+  }
 }
